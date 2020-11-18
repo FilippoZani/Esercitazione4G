5f8aa1380c5cd09e6285f469432f7635fa17fea7
leonardo969 <leonardo.vincenzi.stud@ispascalcomandini.it>
2020-11-18 11:28:53 +0100

    Aggiunta delle proprietà, dei metodi get e set

---
+++ Esercitazione4G/Libro.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Esercitazione4G
+{
+    public class Libro
+    {
+        private string _autore;
+        private string _titolo;
+        private int _annoPubblicazione;
+        private string _editore;
+        private int _nPagine;
+
+        public string Autore
+        {
+            get => default;
+            set
+            {
+                _autore = value;
+            }
+        }
+
+        public string Titolo
+        {
+            get => default;
+            set
+            {
+                _titolo = value;
+            }
+        }
+
+        public int AnnoPubblicazione
+        {
+            get => default;
+            set
+            {
+                _annoPubblicazione = value;
+            }
+        }
+
+        public string Editore
+        {
+            get => default;
+            set
+            {
+                _editore = value;
+            }
+        }
+
+        public int NPagine
+        {
+            get => default;
+            set
+            {
+                _nPagine = value;
+            }
+        }
+
+        
+    }
+}
